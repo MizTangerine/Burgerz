6d6243cfb0e99ba42eec162db43341a0ac5c572e
Clarissa Reeve <clarissa.reeve@gmail.com>
2020-12-12 14:30:29 -0700

    fixed proper case function

--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -3,7 +3,7 @@ $(() => {
     $(".create").on("submit", (event) => {
         event.preventDefault();
         let newBurger = {
-            burger_name: $("#newBurger").val().trim().split(' ').map(word => word.substr(0, 1).toUpperCase() + word.substr(1)).join(' ')
+            burger_name: $("#newBurger").val().toLowerCase().trim().split(' ').map(word => word.substr(0, 1).toUpperCase() + word.substr(1)).join(' ')
         }
         // console.log(newBurger);
         $.ajax("/api/burgers", {
